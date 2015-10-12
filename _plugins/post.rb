99768cc383af494a289fc3354274e57d2134358c
Vladimir Sazhin <vladimir.sazhin@gmail.com>
2015-10-12 23:46:17 +0500

    fixed disqus identifier

--- _plugins/post.rb
+++ _plugins/post.rb
@@ -5,5 +5,11 @@ module Jekyll
     def url_placeholders
       old_url_placeholders.merge(categories: (categories || []).map { |c| Utils.slugify(c.to_s) }.join('/'))
     end
+
+    alias_method :old_id, :id
+
+    def id
+      CGI::unescape(url).sub(/\A\//, '').sub(/\/\z/, '')
+    end
   end
 end
